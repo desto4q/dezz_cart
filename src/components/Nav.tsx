f65e41e45cc554bbf89dd13727c48d56e1ad0be8
Destiny <desto4q@gmail.com>
2024-01-30 22:24:06 +0100

    reponsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,51 +1,49 @@
-import {IconSearch, IconShoppingBag } from '@tabler/icons-react';
+import { IconSearch, IconShoppingBag } from "@tabler/icons-react";
+import { useState } from "react";
 
+import { Spin as Hamburger } from "hamburger-react";
+import { nav_links } from "../data/data";
+import { Link } from "wouter";
 function Nav() {
-    return (
-        <nav>
-            <div className="strict_width">
-                <div className="logo">
-                    ConForm
-                </div>
-                <div className="links">
-                    <div className="left">ConForm</div>
-                    <span>
-                        <a href="#">
-                            Home
-                        </a>
-                        <a href="#">
-                            Shop
-                        </a>
-                        <a href="#">
-                          Product
-                        </a>
-                        <a href="#">
-                            Categories
-                        </a>
-                        <a href="#">
-                            Blog
-                        </a>
-                        <a href="#">
-                            Pages
-                        </a>
-                        <a href="#">
-                            Shortcode
-                        </a>
-                    </span>
-                    <div className="cart">
-                        <a href='#' className="icon">
-                            <IconShoppingBag/>
-                        </a>
-                        <a href='#' className="search">
-                            <IconSearch/>
-                        </a>
-                    </div>
+	let [sideNav, SetSideNav] = useState<boolean>(false);
+	return (
+		<nav>
+			<div className={`sideNav ${sideNav ? "sideNav_active" : null}`}>
+				<div className="hamburger_cont_Side">
+					<Hamburger size={28} toggled={sideNav} toggle={SetSideNav} />
+				</div>
+			</div>
+			<div className="strict_width">
+				<div className="logo">ConForm</div>
 
-                </div>
+				<div className="links">
+					<div className="left">ConForm</div>
+					<span>
+						{nav_links.map(({ text, path }) => {
+							return (
+								<Link to={path} key={text}>
+									{text}
+								</Link>
+							);
+						})}
+					</span>
 
-            </div>
-        </nav>
-    )
+					<div className="cart">
+						<a href="#" className="icon">
+							<IconShoppingBag />
+						</a>
+						<a href="#" className="search">
+							<IconSearch />
+						</a>
+					</div>
+
+					<div className="hamburger_cont">
+						<Hamburger size={28} toggled={sideNav} toggle={SetSideNav} />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
-export default Nav
+export default Nav;
---
+++ src/data/data.tsx
@@ -0,0 +1,7 @@
+export const nav_links: { text: string; path: string }[] = [
+	{ text: "Home", path: "/" },
+	{ text: "Products", path: "/" },
+	{ text: "Categories", path: "/categories" },
+	{ text: "Blog", path: "/" },
+];
+
